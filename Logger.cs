d817f497ece40f6a4f037f5439c9dbec2d9d9162
LunaBroeke <onefivefoursakura@gmail.com>
2024-09-09 09:20:58 +0200

    Initial commit

---
+++ Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovementSystemServer
+{
+    public static class Logger
+    {
+        public static void Log(string s) { Console.ForegroundColor = ConsoleColor.White;  Console.WriteLine(s); }
+
+        public static void LogWarning(string s) { Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine(s); }
+
+        public static void LogError(string s) { Console.ForegroundColor = ConsoleColor.Red;  Console.WriteLine(s); }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace MovementSystemServer
+{
+    public class Program
+    {
+        TcpListener server;
+        public static void Main(string[] args) => new Program().StartServer();
+
+        public void StartServer()
+        {
+            try
+            {
+                IPAddress address = IPAddress.Any;
+                int port = 37484;
+
+                server = new TcpListener(address, port);
+
+                server.Start();
+
+                Byte[] bytes = new byte[256];
+                string data = null;
+
+                while (true)
+                {
+                    Logger.Log("Waiting for connection..");
+
+                    using TcpClient client = server.AcceptTcpClient();
+                    Logger.Log("Connected!");
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Logger.Log($"Received: {data}");
+
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        stream.Write(msg, 0, msg.Length);
+                        Logger.Log($"sent {data}");
+                    }
+                }
+            }
+            catch(Exception e) { Logger.LogError(e.ToString()); }
+            finally { server.Stop(); }
+            Logger.Log("Hit any key to continue");
+            Console.Read();
+        }
+    }
+}
